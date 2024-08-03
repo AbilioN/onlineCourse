0115ec034f352a54d880a282e2ef7f03f69d2d5c
AbilioN <netobalby@gmail.com>
2024-08-03 21:53:37 +0200

    add safearea and margin

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -8,41 +8,49 @@ class Welcome extends StatelessWidget {
   final PageController _controller = PageController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          PageView(
-            controller: _controller,
-            scrollDirection: Axis.horizontal,
-            children: [
-              OnboardingPageOne(
-                controller: _controller,
-                imagePath: "assets/images/reading.png",
-                title: "First see Learning",
-                subTitle:
-                    "Forget about all paper all knolege in one learning putting a litle bit of more text",
-                index: 1,
-              ),
-              OnboardingPageOne(
-                controller: _controller,
-                imagePath: "assets/images/man.png",
-                title: "Connect with Everyone",
-                subTitle:
-                    "Allways keep in touch with your tutor and friends. Lets get connected.",
-                index: 2,
-              ),
-              OnboardingPageOne(
-                controller: _controller,
-                imagePath: "assets/images/boy.png",
-                title: "Always facinated learning",
-                subTitle:
-                    "Anywhere and anytime at your descretion. So study wherever you can",
-                index: 3,
-              ),
-            ],
+    return Container(
+      // color: Colors.red,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Container(
+            margin: const EdgeInsets.only(top: 30),
+            child: Stack(
+              children: [
+                PageView(
+                  controller: _controller,
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    OnboardingPageOne(
+                      controller: _controller,
+                      imagePath: "assets/images/reading.png",
+                      title: "First see Learning",
+                      subTitle:
+                          "Forget about all paper all knolege in one learning putting a litle bit of more text",
+                      index: 1,
+                    ),
+                    OnboardingPageOne(
+                      controller: _controller,
+                      imagePath: "assets/images/man.png",
+                      title: "Connect with Everyone",
+                      subTitle:
+                          "Allways keep in touch with your tutor and friends. Lets get connected.",
+                      index: 2,
+                    ),
+                    OnboardingPageOne(
+                      controller: _controller,
+                      imagePath: "assets/images/boy.png",
+                      title: "Always facinated learning",
+                      subTitle:
+                          "Anywhere and anytime at your descretion. So study wherever you can",
+                      index: 3,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
