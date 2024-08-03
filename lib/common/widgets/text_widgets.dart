5da74a8c6e739af9dcbc8a443578c118865bb7b6
AbilioN <netobalby@gmail.com>
2024-08-03 15:33:45 +0200

    add utils

--- lib/common/widgets/text_widgets.dart
+++ lib/common/widgets/text_widgets.dart
@@ -1,16 +1,22 @@
+import 'package:another/common/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class Text24 extends StatelessWidget {
   final String text;
-  const Text24({super.key, required this.text});
+  final Color color;
+  const Text24({
+    super.key,
+    required this.text,
+    this.color = AppColors.primaryText,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
       textAlign: TextAlign.center,
-      style: const TextStyle(
-        color: Colors.blue,
+      style: TextStyle(
+        color: color,
         fontSize: 24,
         fontWeight: FontWeight.normal,
       ),
@@ -20,15 +26,20 @@ class Text24 extends StatelessWidget {
 
 class Text16 extends StatelessWidget {
   final String text;
-  const Text16({super.key, required this.text});
+  final Color color;
+  const Text16({
+    super.key,
+    required this.text,
+    this.color = AppColors.primarySecondaryElementText,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
       textAlign: TextAlign.center,
-      style: const TextStyle(
-        color: Colors.blue,
+      style: TextStyle(
+        color: color,
         fontSize: 16,
         fontWeight: FontWeight.normal,
       ),
