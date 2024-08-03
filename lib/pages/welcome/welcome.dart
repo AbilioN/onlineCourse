b55a3c2c6f8b13eb958fc5ceab542b5a312ff7b0
AbilioN <netobalby@gmail.com>
2024-08-03 21:09:33 +0200

    page index controller

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -3,7 +3,9 @@ import 'package:another/pages/welcome/widgets.dart';
 import 'package:flutter/material.dart';
 
 class Welcome extends StatelessWidget {
-  const Welcome({super.key});
+  Welcome({super.key});
+
+  final PageController _controller = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,24 +13,31 @@ class Welcome extends StatelessWidget {
       body: Stack(
         children: [
           PageView(
+            controller: _controller,
             scrollDirection: Axis.horizontal,
             children: [
               OnboardingPageOne(
+                controller: _controller,
                 imagePath: "assets/images/reading.png",
                 title: "First see Learning",
                 subTitle: "Forget about all paper all knolege in one learning",
+                index: 1,
               ),
               OnboardingPageOne(
+                controller: _controller,
                 imagePath: "assets/images/man.png",
                 title: "Connect with Everyone",
                 subTitle:
                     "Allways keep in touch with your tutor and friends. Lets get connected.",
+                index: 2,
               ),
               OnboardingPageOne(
+                controller: _controller,
                 imagePath: "assets/images/boy.png",
                 title: "Always facinated learning",
                 subTitle:
                     "Anywhere and anytime at your descretion. So study wherever you can",
+                index: 3,
               ),
             ],
           ),
--- lib/pages/welcome/widgets.dart
+++ lib/pages/welcome/widgets.dart
@@ -7,12 +7,17 @@ class OnboardingPageOne extends StatelessWidget {
   final String imagePath;
   final String title;
   final String subTitle;
+  final PageController controller;
+
+  final int index;
 
   const OnboardingPageOne({
     super.key,
+    required this.controller,
     required this.imagePath,
     required this.title,
     required this.subTitle,
+    this.index = 0,
   });
 
   @override
@@ -33,30 +38,45 @@ class OnboardingPageOne extends StatelessWidget {
             text: subTitle,
           ),
         ),
-        _NextButton()
+        _NextButton(
+          controller: controller,
+          index: index,
+        )
       ],
     );
   }
 }
 
 class _NextButton extends StatelessWidget {
-  const _NextButton();
+  final int index;
+  final PageController controller;
+  const _NextButton({required this.controller, required this.index});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 325,
-      height: 50,
-      margin: const EdgeInsets.only(
-        top: 100,
-        left: 25,
-        right: 25,
-      ),
-      decoration: AppBoxDecorations.customBoxDecoration(),
-      child: const Center(
-        child: Text16(
-          text: "Next",
-          color: AppColors.primaryBackground,
+    return GestureDetector(
+      onTap: () {
+        print('index $index');
+
+        if (index < 3) {
+          controller.animateToPage(index,
+              duration: Duration(milliseconds: 300), curve: Curves.bounceIn);
+        }
+      },
+      child: Container(
+        width: 325,
+        height: 50,
+        margin: const EdgeInsets.only(
+          top: 100,
+          left: 25,
+          right: 25,
+        ),
+        decoration: AppBoxDecorations.customBoxDecoration(),
+        child: const Center(
+          child: Text16(
+            text: "Next",
+            color: AppColors.primaryBackground,
+          ),
         ),
       ),
     );
